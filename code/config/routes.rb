5e057e3b850be1cdfc21499d1d162cd240c20148
Madeline Febinger <febingm1@tcnj.edu>
2018-11-19 21:38:30 -0500

    Created user accounts and login page with devise gem

--- code/config/routes.rb
+++ code/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'home#index'
   get '/about' => 'pages#about'
   get '/friends' => 'pages#friends'
   get '/request' => 'pages#request'
