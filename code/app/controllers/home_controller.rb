2dc752cdec271ed060cf6785de699fc43cc732a0
Madeline Febinger <febingm1@tcnj.edu>
2018-11-24 15:38:33 -0500

    Modified the home page to show sorted lists of scam reports from friends and all users

--- code/app/controllers/home_controller.rb
+++ code/app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
 	def index
+		#Get all users for searching purposes
+		@users = User.all
+
+		# Find all scams reported by friends
+		@friend_scams = Array.new
+
+		current_user.friends.each do |friend|
+			friend.scams.each do |scam|
+				@friend_scams.push(scam)
+			end
+		end
+
+		# Sort @friend_scams by most recent
+		@friend_scams.sort_by { |scam| [scam.created_at] }
+
+
+		# Sort all scam reports by most recent
+		@sorted_scams = Scam.all
+
+		@sorted_scams.sort_by { |scam| [scam.created_at] }
 	end
 end
